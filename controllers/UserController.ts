0e06239ae12cdcaffa96ffde890d56cf749f1e3c
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-08-02 16:26:27 +0700

    Add user controller

---
+++ controllers/UserController.ts
@@ -0,0 +1,45 @@
+import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts"
+import {IUser, User} from "./models/index.ts"
+
+export class UserController {
+    async create(values: IUser) {
+        // Call static user method
+        const password = await User.hashPassword(values.password)
+
+        const user: IUser = {
+            firstName: values.firstName,
+            lastName: values.lastName,
+            password
+        }
+
+        await User.create(user as any)
+
+        return user
+    }
+
+    async delete(id: string) {
+        await User.deleteById(id)
+    }
+
+    getAll() {
+        return User.all()
+    }
+
+    getOne(id: string) {
+        return User.where("id", id).first()
+    }
+
+    async update(id: string, values: IUser) {
+        await User.where("id", id).update(values as any)
+        return this.getOne(id)
+    }
+
+    async login(lastName: string, password: string) {
+        const user = await User.where("lastname", lastName).first()
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return false
+        }
+        // TODO generate JWT
+    }
+
+}
